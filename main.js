d3af1272ec235cdaa1f8874063b24e9d52f6db81
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-14 20:42:15 -0500

    readme

--- main.js
+++ main.js
@@ -42,6 +42,56 @@ function selectHtml (field) {
     return selectHtml;
 }
 
+function getData (callback) {
+    $.ajax({
+     url: `http://json-data.herokuapp.com/forms`,
+     dataType: "json",
+     success: callback
+  });
+}
+
+function fillHtml (data) {
+  //debugger;
+    for (var i = 0; i < data.length; i++) {
+    var field = data[i];
+        if (field.type === "select") {
+            var html = selectHtml(field);
+        } else {
+            var html = `
+            <div>
+            <input placeholder="${field.label}"/>
+            </div>
+            `;
+          }
+        $('.forms-container').append(html);
+  }
+}
+
+getData(fillHtml);
+
+function selectHtml (field) {
+        var selectHtml = "";
+   // generate optionsHtml with a loop
+    for (var count = 0; count < 5; count++) {
+
+     selectHtml += `<div class="selectLanguage">
+              <input placeholder="${field.label}"</input>
+              <select name="select">
+              <option value="language">${field.options[count].label}</option>
+              </select>
+              </div>`;
+
+
+   }
+    return selectHtml;
+}
+
+
+
+
+
+
+
 // <select name="select">
 //   <option value="value1">Value 1</option>
 //   <option value="value2" selected>Value 2</option>
